2e8c3aed8b3acaa4cd80c5dcd7a88498bf51f0f7
Celso Mireles <celso@digitalstrategy.tips>
2016-03-01 14:11:53 -0700

    Style #profile-menu

--- client/components/navbar/navbar.jsx
+++ client/components/navbar/navbar.jsx
@@ -1,4 +1,13 @@
 Navbar = React.createClass({
+	mixins: [ReactMeteorData],
+	getMeteorData(){
+		let data = {};
+		data.currentUser = Meteor.user();
+		return data;
+	},
+	handleSignout(e){
+
+	},
 	render(){
 		return (
 			<div>
@@ -10,8 +19,24 @@ Navbar = React.createClass({
 							</div>
 							<div className="col-xs-4 col-sm-12">
 								<p className="text-center">
-									<img src="img/logo.png" className="img-responsive img-center" alt="Cosecha"/>
+									<button type="button" className="btn btn-default center-logo" data-toggle="collapse" data-target="#profile-menu">
+										<img src="img/logo.png" className="img-responsive img-center" alt="Cosecha"/>
+										<i className="fa fa-chevron-down"></i>
+									</button>
 								</p>
+
+								<div className="collapse" id="profile-menu">
+									<nav className=" navbar-collapse nav-justified" aria-expanded="false">
+										<ul className="nav navbar-nav">
+											<li><a href="/profile">Edit Profile</a></li>
+											<li><a href="/signout">Signout</a></li>
+
+										</ul>
+									</nav>
+								</div>
+
+
+
 
 							</div>
 							<div className="col-xs-4 hidden-sm hidden-md hidden-lg">

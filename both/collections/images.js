209fed338f38b70731463aed363a78e9b1573cf9
Celso Mireles <celso@digitalstrategy.tips>
2016-03-02 03:41:13 -0700

    Only allow PNG or JPG files in status form

--- both/collections/images.js
+++ both/collections/images.js
@@ -1,5 +1,23 @@
 Images = new FS.Collection('images', {
-	stores:[new FS.Store.FileSystem('images', {path:'~/uploads'})]
+	stores:[new FS.Store.FileSystem('images', {path:'~/uploads'})],
+	filter: {
+    maxSize: 548576, // in bytes
+    allow: {
+      contentTypes: ['image/*'],
+      extensions: ['png', 'jpg', 'jpeg']
+    },
+    deny: {
+      contentTypes: ['video/mp4'],
+      extensions: ['mp4']
+    },
+  	onInvalid: function (message) {
+      if (Meteor.isClient) {
+        alert(message + '\n\nPlease upload only PNG or JPG files.');
+      } else {
+        console.log(message);
+      }
+    }
+  }
 });
 Images.allow({
 	insert: function(){
